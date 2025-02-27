14cd3b44ffc62aaf9bf4ec43162dc113c035ab27
Manuel Mendoza <developermanuel@icloud.com>
2025-02-26 18:06:49 -0800

    avances

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Produccion
+from .models import Produccion,Orden,Viga
 
 # Register your models here.
 admin.site.register(Produccion)
+admin.site.register(Orden)
+admin.site.register(Viga)
