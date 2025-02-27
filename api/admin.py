97c6916760129e004d8c24a8c9a9827dd3e804c9
Manuel Mendoza <developermanuel@icloud.com>
2025-02-26 17:09:29 -0800

    Base de Datos funcionando

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Produccion
 
 # Register your models here.
+admin.site.register(Produccion)
--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class Produccion(models.Model):
     fecha = models.DateField()
 
