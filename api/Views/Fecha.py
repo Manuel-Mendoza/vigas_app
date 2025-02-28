eaf210954f989dbf7fe2bc04ced77c4f204b5ca7
Manuel Mendoza <joseguns2@gmail.com>
2025-02-27 22:57:04 -0800

    Se puse autoejecutable el POST de Fecha, para que se cree automaticamente

--- api/Views/Fecha.py
+++ api/Views/Fecha.py
@@ -1,9 +1,11 @@
 
+import json
 from django.views import View
 from api.models import Produccion
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from datetime import datetime
 
 class FechaView(View):
     def get(self, request):
@@ -14,12 +16,24 @@ class FechaView(View):
             datos = {'message': 'No se encontraron vigas'}
         return JsonResponse(datos)
 
+
+#-------------------Dispatch-----------------
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
+        #Eliminar para se vaya a subir a la web
+#-------------------------------------------------
+
+
     def post(self, request):
-        # Placeholder for handling POST requests
-        # You can add the logic to handle POST data here
-        response_data = {'message': 'POST request received'}
+        # Se revisa si hay body, si no, simplemente continuamos
+        if request.body:
+            jsondata = json.loads(request.body)
+            # Variable jsondata ahora está disponible para uso futuro si es necesario
+
+        # Usar datetime.now() correctamente
+        fecha_actual = datetime.now().strftime('%m-%d-%Y')
+        Produccion.objects.create(fecha=fecha_actual)
+        response_data = {'message': 'POST request received', 'fecha': fecha_actual}
         return JsonResponse(response_data)
