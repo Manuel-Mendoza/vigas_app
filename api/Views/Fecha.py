ecc925ff463ff874b52fa5282f0ce5d6785d9f6e
Manuel Mendoza <joseguns2@gmail.com>
2025-02-28 18:20:06 -0800

    Resolved merge conflicts in db.sqlite3 and api/Views/Fecha.py

--- api/Views/Fecha.py
+++ api/Views/Fecha.py
@@ -1,11 +1,9 @@
 
-import json
 from django.views import View
 from api.models import Produccion
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from datetime import datetime
 
 class FechaView(View):
     def get(self, request):
@@ -16,17 +14,12 @@ class FechaView(View):
             datos = {'message': 'No se encontraron Fechas'}
         return JsonResponse(datos)
 
-
-#-------------------Dispatch-----------------
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
-        #Eliminar para se vaya a subir a la web
-#-------------------------------------------------
-
-
     def post(self, request):
+<<<<<<< HEAD
         # Se revisa si hay body, si no, simplemente continuamos
         if request.body:
             jsondata = json.loads(request.body)
@@ -36,4 +29,9 @@ class FechaView(View):
         fecha_actual = datetime.now().strftime('%m-%d-%Y')
         Produccion.objects.create(fecha=fecha_actual)
         response_data = {'fecha': fecha_actual}
+=======
+        # Placeholder for handling POST requests
+        # You can add the logic to handle POST data here
+        response_data = {'message': 'POST request received'}
+>>>>>>> parent of eaf2109 (Se puse autoejecutable el POST de Fecha, para que se cree automaticamente)
         return JsonResponse(response_data)
