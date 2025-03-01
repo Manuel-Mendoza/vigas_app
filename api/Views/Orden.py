8dc7142566590a0a39daceda0ac193f4c751faef
Manuel Mendoza <joseguns2@gmail.com>
2025-03-01 14:56:15 -0800

    Trabajando en APIs Ordenes

--- api/Views/Orden.py
+++ api/Views/Orden.py
@@ -1,9 +1,13 @@
 from django.views import View
 from api.models import Orden
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class OrdenView(View):
+
+    @csrf_exempt
     def get(self, request):
         orden = list(Orden.objects.values())
         if len(orden) > 0:
--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     #-------------APIs FechaView-----------------
     path('fecha/',FechaView.as_view(), name='fecha'),
     path('fecha/<int:id>/',FechaView.as_view(), name='deletefecha'),
-    #-------------APIs VigasView-----------------
-    path('vigas/',VigasView.as_view(), name='vigas'),
     #-------------APIs OrdenView-----------------
     path('ordenes/',OrdenView.as_view(), name='ordenes'),
+    #-------------APIs VigasView-----------------
+    path('vigas/',VigasView.as_view(), name='vigas'),
 ]
