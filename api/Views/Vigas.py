b5f15a24e07c6b599cd633b51d02128c1d524285
Manuel Mendoza <joseguns2@gmail.com>
2025-03-02 11:39:59 -0800

    DELETE de Orden funciona

--- api/Views/Vigas.py
+++ api/Views/Vigas.py
@@ -4,13 +4,16 @@ from api.models import Viga
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+@method_decorator(csrf_exempt, name='dispatch')
 class VigasView(View):
 
+#-------------------------------Post-----------------------------
+    @csrf_exempt
     def post(self, request):
         datos = {'message': 'success'}
         return JsonResponse(datos)
-    #________Decorator_________
 
+#-------------------------------Get-----------------------------
     def get(self, request):
         vigas = list(Viga.objects.values())
         if len(vigas) > 0:
