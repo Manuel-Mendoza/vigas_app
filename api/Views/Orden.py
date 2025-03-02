e07476f563f4a1ae48dbe1dda08dd8278793b50f
Manuel Mendoza <joseguns2@gmail.com>
2025-03-01 16:44:42 -0800

    POST ordenes no funciona

--- api/Views/Orden.py
+++ api/Views/Orden.py
@@ -23,17 +23,17 @@ class OrdenView(View):
 
             jsondata = json.loads(request.body)
             if jsondata['numero_orden']:
-                 numero_orden = jsondata['numero_orden']
+                numero_orden = jsondata['numero_orden']
 
             print(numero_orden)
 
-            # #Validando que la numero de orden no exista en la base de datos
-            # if Orden.objects.filter(numero_orden=numero_orden).exists():
-            #     return JsonResponse({'message': 'Numero de orden ya existe'}, status=400)
+            # Validando que la numero de orden no exista en la base de datos
+            if Orden.objects.filter(numero_orden=numero_orden).exists():
+                 return JsonResponse({'message': 'Numero de orden ya existe'}, status=400)
 
-            # # # Crear el registro con el numero de orden
+            # Crear el registro con el numero de orden
             create_orden = Orden.objects.create(numero_orden=numero_orden)
 
             # Devolver la información del registro creado
-            response_data = {'id': create_orden.id, 'numero_orden': create_orden.numero_orden}
+            response_data = {'id': create_orden.id,'produccion_id': create_orden.produccion_id,'numero_orden':create_orden.numero_orden}
             return JsonResponse(response_data, status=201)
