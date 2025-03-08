15d07280c1be30836ee1259b51e5d186f68a453a
Manuel <joseguns2@gmail.com>
2025-03-07 22:11:36 -0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import OrdenViewSet  # Nota el punto antes de views
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from .views import OrdenViewSet
-
+# Configura el router para ViewSets
 router = DefaultRouter()
 router.register(r'ordenes', OrdenViewSet)
 
 urlpatterns = [
+    # Incluye las URLs generadas por el router
+    path('', include(router.urls)),
+
     # URLs para la documentación de la API
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Interfaz Swagger UI opcional
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # Interfaz ReDoc opcional
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
