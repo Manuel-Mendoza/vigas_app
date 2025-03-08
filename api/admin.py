43cd394f2e5d752c8b3c0c077dc0ff14404d56a9
Manuel <joseguns2@gmail.com>
2025-03-07 19:20:24 -0800

    Base de Datos

--- api/admin.py
+++ api/admin.py
@@ -3,10 +3,10 @@ from .models import Orden, Viga
 
 @admin.register(Orden)
 class OrdenAdmin(admin.ModelAdmin):
-    list_display = ("id", "numero", "fecha")  # Usando los nombres correctos de los campos
-    search_fields = ("numero",)  # Permite búsqueda por número de orden
+    list_display = ("id", "numero_orden", "fecha")  # Usando los nombres correctos de los campos
+    search_fields = ("numero_orden",)  # Permite búsqueda por número de orden
 
 @admin.register(Viga)
 class VigaAdmin(admin.ModelAdmin):
-    list_display = ("id", "nombre", "cantidad", "medidas", "orden")  # Ajusta según los campos de Viga
-    search_fields = ("nombre", "orden__numero")  # Corregido para usar el nombre correcto del campo
+    list_display = ("id", "nombre", "cantidad", "medidas", "orden")
+    search_fields = ("nombre", "orden__numero")
--- api/models.py
+++ api/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Orden(models.Model):
-    numero = models.CharField(max_length=10, unique=True)
+    numero_orden = models.CharField(max_length=10, unique=True)
     fecha = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return f"Orden {self.numero} - {self.fecha}"
+        return f"Orden {self.numero_orden} - {self.fecha}"
 
 class Viga(models.Model):
     orden = models.ForeignKey(Orden, related_name="vigas", on_delete=models.CASCADE)
