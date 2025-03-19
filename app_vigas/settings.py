fc6265e4f9cf76dc3232e9ac38dcf450a82321cc
Manuel <joseguns2@gmail.com>
2025-03-19 16:39:23 -0700

    Update settings.py

--- app_vigas/settings.py
+++ app_vigas/settings.py
@@ -140,7 +140,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost:4321",  # Ensure this is included
 ]
-CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "https://vigasapp-production.up.railway.app",
 ]
